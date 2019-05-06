b50c55c364a531ec4d76bea634cbe263a789a712
Hazel Lain Snider <hazysnakes@gmail.com>
2019-05-06 11:34:29 -0400

    Simplify player

--- player/Player.gd
+++ player/Player.gd
@@ -1,12 +1,18 @@
 extends KinematicBody2D
 class_name Player
 
-export (int) var MAX_FALL_SPEED
+export (int) var WALK_SPEED = 60
+export (int) var JUMP_HEIGHT = 8
+export (int) var JUMP_SPEED = 80
+export (int) var MAX_FALL_SPEED = 200
+
+onready var animation = $AnimationPlayer
 
 var gravity = 200
 var horizontal_damping = 0.2
-var velocity = Vector2()
-var snap = Vector2(0,12)
+var direction = "left"
+var velocity = Vector2(80, -100)
+var snap = Vector2(0,6)
 
 var states = {}
 var state_stack = []
@@ -14,18 +20,23 @@ var state_stack = []
 func _ready():
 	var PlayerStates = load("res://player/PlayerStates.gd")
 	states = {
-		"Idle": PlayerStates.Idle.new(self),
-		"Walk": PlayerStates.Walk.new(self),
-		"JumpCharge": PlayerStates.JumpCharge.new(self),
+		"Ground": PlayerStates.Ground.new(self),
 		"Air": PlayerStates.Air.new(self),
+		"Jump": PlayerStates.Jump.new(self),
 	}
-	state_stack.push_front(states["Idle"])
+	state_stack.push_front(states["Ground"])
+
+func _process(delta):
+	if direction == "left":
+		$Sprite.flip_h = true
+	else: $Sprite.flip_h = false
 
 func _physics_process(delta):
 	state_stack[0].update(delta)
 	
-	velocity = move_and_slide_with_snap(velocity, snap, Vector2(0,-1),
-		true, 4, 0.523599, true)
+	if velocity.y < MAX_FALL_SPEED:
+		velocity.y += gravity * delta
+	velocity = move_and_slide_with_snap(velocity, snap, Vector2(0,-1))
 	
 	velocity.x *= pow(horizontal_damping, delta*4)
 
--- player/PlayerStates.gd
+++ player/PlayerStates.gd
@@ -1,10 +1,15 @@
-class Idle:
+class Ground:
 	var player: Player
-	func _init(p: Player): player = p
+	func _init(p): player = p
 
 	# Called on state entered
 	func enter():
-		player.horizontal_damping = 0.2
+		player.horizontal_damping = 0.05
+		player.snap = Vector2(0,6)
+		player.gravity = 2000
+		
+		player.animation.play("stand")
+		print("Ground")
 
 	# Called on state exited
 	func exit():
@@ -12,67 +17,39 @@ class Idle:
 
 	# Handle Input
 	func input(event: InputEvent):
-		if event.is_action_pressed("left"):
-			player.change_state("Walk")
 		if event.is_action_pressed("jump"):
-			player.change_state("JumpCharge")
+			player.change_state("Jump")
 
 	# Handle physics
 	func update(delta):
-		print("State: Idle")
 		if not player.is_on_floor():
 			player.change_state("Air")
-
-class Walk:
-	var player: Player
-
-	func _init(p: Player): player = p
-
-	# Called on state entered
-	func enter():
-		pass
-
-	# Called on state exited
-	func exit():
-		pass
-
-	# Handle Input
-	func input(event):
-		pass
-
-	# Handle physics
-	func update(delta):
-		print("State: Walk")
-
-class JumpCharge:
-	var player: Player
-	func _init(p: Player): player = p
-
-	# Called on state entered
-	func enter():
-		pass
-
-	# Called on state exited
-	func exit():
-		pass
-
-	# Handle Input
-	func input(event):
-		pass
-
-	# Handle physics
-	func update(delta):
-		print("State: JumpCharge")
+		
+		var walk = 0
+		if Input.is_action_pressed("right"):
+			player.direction = "right"
+			walk += player.WALK_SPEED
+		if Input.is_action_pressed("left"):
+			player.direction = "left"
+			walk -= player.WALK_SPEED
+		if walk != 0:
+			player.animation.play("walk")
+			player.velocity.x = walk
+		else:
+			player.animation.play("stand")
 
 class Air:
 	var player: Player
-	var airtime
-
-	func _init(p: Player): player = p
+	func _init(p): player = p
 
 	# Called on state entered
 	func enter():
-		player.horizontal_damping = 0.8
+		player.horizontal_damping = 0.9
+		player.snap = Vector2(0,6)
+		player.gravity = 200
+		
+		player.animation.play("air")
+		print("Air")
 
 	# Called on state exited
 	func exit():
@@ -84,9 +61,43 @@ class Air:
 
 	# Handle physics
 	func update(delta):
-		print("State: Air")
-		airtime += delta
+		player.animation.play("air")
 		if player.is_on_floor():
-			airtime = 0
-			player.change_state("Idle")
-		player.velocity.y += player.gravity * delta
+			player.change_state("Ground")
+		
+		if Input.is_action_pressed("left"):
+			player.velocity.x -= player.WALK_SPEED * delta
+		if Input.is_action_pressed("right"):
+			player.velocity.x += player.WALK_SPEED* delta
+
+class Jump:
+	var player: Player
+	func _init(p): player = p
+	
+	var height
+
+	# Called on state entered
+	func enter():
+		player.horizontal_damping = 0.9
+		player.snap = Vector2(0,0)
+		player.gravity = 200
+		
+		player.animation.play("jump")
+		height = player.position.y - player.JUMP_HEIGHT
+		player.velocity.y = -player.JUMP_SPEED
+
+	# Called on state exited
+	func exit():
+		pass
+
+	# Handle Input
+	func input(event):
+		pass
+
+	# Handle physics
+	func update(delta):
+#		if player.position.y > height:
+#			player.velocity.y = -player.JUMP_SPEED
+
+		if player.velocity.y > 0 or player.is_on_ceiling():
+			player.change_state("Air")
